27f114a055105d3f13da87f2c76ce7adf719bb3c
nulldeezmon <null@nulldaemon.eu.org>
2026-03-10 22:15:16 +0700

    Update door.sh

--- door.sh
+++ door.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-if [[ "$REVISION" == "UNRELEADED_0x76a9a3fed017f1e1676b53f62891c1a3" ]]; then # idc if this is a misspell
+if [[ "$REVISION" == "UNRELEASED_0x76a9a3fed017f1e1676b53f62891c1a3" ]]; then # cool hex
             echo "You are running an unreleased build, this could be SPECIFICALLY stopped from running."
 fi
 
